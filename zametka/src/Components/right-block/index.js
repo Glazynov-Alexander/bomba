878d19affb761807cc9182cde5317f048114cfa8
Glazynov-Alexander <https://github.com/Glazynov-Alexander/zametky.git>
2021-01-06 17:58:50 +0300

    zametky-refactor-style

--- zametka/src/Components/right-block/index.js
+++ zametka/src/Components/right-block/index.js
@@ -1,30 +1,46 @@
-import React from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import './style.css'
-let RightBlock = (props) => {
-    function upText (e, text) {
-       if( e.type === "blur") {
-           props.upBody(props.task[0]._id, text)
-           props.upOpacity(false)
-       }
-    }
 
+let RightBlock = (props) => {
     if (!props.task || !props.task[0]) {
         return null
     }
+    let flag = false
+    let [text, updateText] = useState(props.task[0].text)
+    let [one, upOne] = useState(2)
+    const titleRef = useRef();
+
+    function upText(e) {
+        if (e.code === "ShiftLeft" || e.code === "ShiftRight") flag = true
+        if (e.code === "Enter" && flag) {}
+
+        if (!flag && (e.type === "blur" || e.code === "Enter")) {
+            props.upBody(props.task[0]._id, e.target.value.replace(/\n/g, "<br/>"))
+            props.upOpacity(false)
+        }
+    }
+
+    useEffect(() => {
+        if (titleRef.current) {
+            titleRef.current.innerHTML = `<div >${props.task[0].text}</div>`
+        }
+    }, [props.task, one])
 
     return (
         <div className="rightBlock">
-           <h2>{props.task[0].title}</h2>
+            <h2>{props.task[0].title}</h2>
             {
-                props.position || !props.task[0].text ? <textarea style={{width:"600px", height:"300px"}} autoFocus={true}
-                                        onBlurCapture={(e) => upText(e, e.target.value)}
-                                        onKeyDown={e => upText(e, e.target.value)}
-                                        type="text" defaultValue={props.task[0].text}/>
-                    :<div><ol>
-                    <li><div style={{wordWrap: "break-word", width:"900px"}} >{props.task[0].text}</div></li>
-                    </ol></div>
+                props.position || !props.task[0].text ?
+                    <textarea style={{width: "600px", height: "300px"}} autoFocus={true}
+                              onChange={e => updateText(e.target.value)}
+                              onBlurCapture={upText}
+                              rows="10" cols="20" wrap="hard"
+                              onKeyDown={upText}
+                              type="text" defaultValue={text.replaceAll("<br/>", " \n")}/>
+                    : <div style={{width: "900px"}} ref={titleRef}></div>
+
             }
         </div>
     )
 }
-export  default  RightBlock
+export default RightBlock
